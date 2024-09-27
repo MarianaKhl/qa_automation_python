8fad9a0b75f2ccdf653d35e65be009a58ce2816b
marianna <m.khlivnenko@gmail.com>
2024-09-28 00:19:22 +0200

    Added homework_13

---
+++ lesson_13/compare_files_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def test_compare_csv_files():
+    # download two CSV files
+    file1 = 'https://raw.githubusercontent.com/dntpanix/automation_qa/main/ideas_for_test/work_with_csv/random.csv'
+    file2 = 'https://raw.githubusercontent.com/dntpanix/automation_qa/main/ideas_for_test/work_with_csv/random-michaels.csv'
+
+    # reading files
+    rf1 = pd.read_csv(file1)
+    rf2 = pd.read_csv(file2)
+
+    # pd.concat() allows you to merge two files into one dataframe
+    unit_files_1_2 = pd.concat([rf1, rf2])  # [df1, df2] — це список, що містить два датафрейми df1 і df2
+
+    # delete duplicate
+    result_delete_duplicate = unit_files_1_2.drop_duplicates()
+
+    # safe result in new csv file
+    new_file = 'khlivnenko.csv'
+    result_delete_duplicate.to_csv(new_file, index=False)
+
+
+
---
+++ lesson_13/invalidate_files_json.py
@@ -0,0 +1,27 @@
+import os
+import json
+import logging
+
+# logging
+logging.basicConfig(filename='json__khlivnenko.log', level=logging.ERROR,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def validate_json_files(directory):
+    # go through all the files in the specified directory
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):  # checks if the filename ends on .json
+            # creates the full path to the file by combining the directory and filename
+            filepath = os.path.join(directory, filename)
+            try:
+                # open and check JSON file
+                with open(filepath, 'r', encoding='utf-8') as f:
+                    json.load(f)
+                print(f"{filename} - valid JSON file")
+            except json.JSONDecodeError as e:
+                logging.error(f"Error in the file {filename}: {str(e)}")
+                print(f"{filename} - Invalid JSON file")
+
+json_directory = '/Users/marianna/PycharmProjects/demo/automation_qa/ideas_for_test/work_with_json'
+
+validate_json_files(json_directory)
