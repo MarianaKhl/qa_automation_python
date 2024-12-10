197c940957adfcb7734fc6b889846b7b50541e47
marianna <m.khlivnenko@gmail.com>
2024-12-10 21:48:06 +0100

    Added fix

--- lesson_31_Jenkins/run_tests.sh
+++ lesson_31_Jenkins/run_tests.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# make file executable = chmod +x run_tests.sh
-# start run = ./run_tests.sh
 
-# clearing previous results
 rm -rf allure-results/*
 rm -rf allure-report/*
 
-# start tests
 pytest --alluredir=allure-results
 
-# generation report
 allure generate allure-results -o allure-report --clean
 
-# open report
 allure open allure-report
 
 exit 0
+
